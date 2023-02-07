4df3065defd762f322a9d3ff21803eab720c4622
AngeTia <tiaange03@gmail.com>
2023-02-07 09:11:23 +0000

    Update to get clean code

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
-<html> 
-  <head> 
+<html>
+  <head>
     <title>DailyChallenge</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   </head>
-  <body> 
+  <body>
     <table  width="400px"  height="250px" cellspacing="0px" cellpadding="0px"  border ="3px">
       <?php
         // initialiser le nombre de ligne et l'incrementer  et affiche aussi le nombre de ligne avec tr
-        for($ligne=1;$ligne<=8;$ligne++)
-        { 
+        for ($ligne=1; $ligne<=8; $ligne++) {
           echo "<tr>";
-          // initialiser le nombre de colonne et l'incrementer 
-          for($col=1;$col<=8;$col++)
-          {
+          // initialiser le nombre de colonne et l'incrementer
+          for ($col=1; $col<=8; $col++) {
             $total=$ligne+$col;
             //$total%2==0 permet de diviser les cases en deux deux en fonction des couleur
-            if($total%2==0)
-            {
+            if ($total%2==0) {
               // cellule en couleur blanc definir avec <td>
               echo "<td height=30px width=30px bgcolor=white></td>";
-            }
-            else
-            {
+            } else {
               // cellule en couleur noir definir avec <td>
               echo "<td height=30px width=30px bgcolor=black></td>";
             }
